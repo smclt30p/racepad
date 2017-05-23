23d2cf7f1d5eb8b0ea8992ff64bb63122082a05f
Ognjen Galić <smclt30p@gmail.com>
2017-05-23 22:53:10 +0200

    Fix screen locking

--- Racepad/src/Main.ts
+++ Racepad/src/Main.ts
@@ -25,13 +25,21 @@
 
         }
 
-		Windows.UI.WebUI.WebUIApplication.addEventListener("enteredbackground", () => {
-            this.speedometer.backupOdo();
-            try { this.displayHandler.requestRelease(); } catch (e) { console.log("Exception thrown in release: " + e); }
-		});
-
+        /*
+         * This does not need to be relased as the system does that by itself when
+         * the user leaves the app. The release method is used when there needs to be
+         * dynamic display requests, for example when a e-book is opened in Edge -- the
+         * screen should stay on in the ebook but not always, that's when it gets released.
+         * We dont to that because the display MUST always stay on while inside the app.
+         *
+         * More info: https://docs.microsoft.com/en-us/uwp/api/windows.system.display.displayrequest
+        */
 		Windows.UI.WebUI.WebUIApplication.addEventListener("leavingbackground", () => {
-            try { this.displayHandler.requestActive(); } catch (e) { console.log("Exception thrown in request: " + e); }
+            try {
+                this.displayHandler.requestActive();
+            } catch (e) {
+                console.log("Error requesting displayHandler active: " + e);
+            }
 		});
 
         this.speedometer.restoreOdo();
