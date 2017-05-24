01d06b43f9a2016f323f23dca48c8d431f62a508
Ognjen Galić <smclt30p@gmail.com>
2017-05-24 18:38:34 +0200

    Hopefully fix screen on. Fixes #1

--- Racepad/src/Main.ts
+++ Racepad/src/Main.ts
@@ -17,23 +17,7 @@
 
     public main(): void {
 
-
-        /*
-        * This does not need to be relased as the system does that by itself when
-        * the user leaves the app. The release method is used when there needs to be
-        * dynamic display requests, for example when a e-book is opened in Edge -- the
-        * screen should stay on in the ebook but not always, that's when it gets released.
-        * We dont to that because the display MUST always stay on while inside the app.
-        *
-        * More info: https://docs.microsoft.com/en-us/uwp/api/windows.system.display.displayrequest
-        */
-        Windows.UI.WebUI.WebUIApplication.addEventListener("leavingbackground", () => {
-            try {
-                self.displayHandler.requestActive();
-            } catch (e) {
-                console.log("Error requesting displayHandler active: " + e);
-            }
-        });
+        this.displayHandler.requestActive();
 
         let self = this;
 
